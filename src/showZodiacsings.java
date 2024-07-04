b99f584176755901f65a6792e1161b95df4799eb
Phyo <phyo@example.com>
2024-07-04 09:23:19 +0630

    Push project

--- src/showZodiacsings.java
+++ src/showZodiacsings.java
@@ -5,20 +5,27 @@ public class showZodiacsings {
     public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
         String ans = null;
-        try {
-            do {
-                System.out.print("Enter your birthday month :");
-                int month = sc.nextInt();
-                determinedate(month);
-                System.out.println("Do you want to know again?(Yes or No)");
-                ans = sc.next();
-                if (!ans.equals("Yes") && !ans.equals("yes")) {
-                    System.out.println("Thanks!");
-                }
-            } while (ans.equals("Yes") || ans.equals("yes"));
-        }catch(Exception ex){
-            System.out.println("Error! Make sure your input!");
-        }
+
+        do {
+            try {
+                do {
+                    System.out.print("Enter your birthday month :");
+                    int month = sc.nextInt();
+                    determinedate(month);
+                    System.out.println("Do you want to know again?(Yes or No)");
+                    ans = sc.next();
+                    if (!ans.equals("Yes") && !ans.equals("yes")) {
+                        System.out.println("Thanks!");
+                    }
+                } while (ans.equals("Yes") || ans.equals("yes"));
+            } catch (Exception ex) {
+                System.out.println("Error! Make sure your input!" );
+                sc.nextLine();
+                ans = "Yes";
+
+            }
+        }while (ans.equals("Yes"));
+
 
     }
 
